273aa7ad39cd773ab8beda02d85103585b17b4bd
Rita Castro <ritamcastro@gmail.com>
2024-10-20 18:10:48 +0100

    🧪 Add playwright with the first failing test

---
+++ e2e/todo-app.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+
+test("01 - Initial Render for the ToDo app", async ({ page }) => {
+  await page.goto("/");
+
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/banner_role
+  const header = page.getByRole("banner")
+  await expect(header).toBeVisible()
+  await expect(header.getByText("To-Do in React")).toBeVisible()
+
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/main_role
+  const main = page.getByRole("main")
+  await expect(main).toBeVisible()
+
+  const checkbox = main.getByRole("checkbox")
+  await expect(checkbox).toBeVisible()
+  await expect(checkbox).not.toBeChecked()
+
+  const todoText = main.getByPlaceholder("Add a new todo")
+  await expect(todoText).toBeVisible()
+
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/contentinfo_role
+  const footer = page.getByRole("contentinfo")
+  await expect(footer).toBeVisible()
+})
+
