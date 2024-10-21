786745f80fa0078067dc85893e7c928eb64f0109
Rita Castro <ritamcastro@gmail.com>
2024-10-21 11:36:42 +0100

    ✅ Display todo in a different style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-
+import {useState } from "react"
 const App = () => {
-  return (
-    <>
+
+  const [isChecked, setIsChecked] = useState(false)
+  
+
+  return  <>
     <header>To-Do in React</header>
     <main>
-      <input type="checkbox"/>
-      <input type="text" placeholder='Add a new todo'/>
+      <input type="checkbox" 
+      checked={isChecked} 
+      onChange={() => {setIsChecked(!isChecked)}}
+      />
+      <input type="text" placeholder='Add a new todo'
+      style={{textDecoration: isChecked ? 'line-through' : 'none' }}/>
     </main>
     <footer>Made with 💜</footer>
     </>
-  )
+
 }
 
 // The same as this one ;)
