e959df260b19d90c6289bb07ee18853698b63db8
Rita Castro <ritamcastro@gmail.com>
2024-10-22 17:45:26 +0100

    ✅ Add new todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,50 @@
-import {useState } from "react"
+import { useState } from "react"
+
 const App = () => {
 
-  const [isChecked, setIsChecked] = useState(false)
-  
+  const [items, setItems] = useState([{ id: Date.now(), isDone: false, text: "" }])
 
-  return  <>
+  // Attention on () and returns, thanks GPT! ;)
+  const onTextChange = (id, value) => {
+    const updatedItems = items.map((item) => (item.id === id ? { ...item, text: value } : item))
+
+    setItems(updatedItems)
+  }
+
+  const onToggleItem = (id) => {
+    const updatedItems = items.map((item) => {
+      return item.id === id ? { ...item, isDone: !item.isDone } : item
+    })
+
+    setItems(updatedItems)
+  }
+  const addTodo = () => {
+    setItems([...items, { id: Date.now(), isDone: false, text: "" }])
+  }
+
+  return <>
     <header>To-Do in React</header>
     <main>
-      <input type="checkbox" 
-      checked={isChecked} 
-      onChange={() => {setIsChecked(!isChecked)}}
-      />
-      <input type="text" placeholder='Add a new todo'
-      style={{textDecoration: isChecked ? 'line-through' : 'none' }}/>
+      <button onClick={() => addTodo()}>New</button>
+      <div role="list">
+        {items.map((item) => {
+          return <div
+            role="listitem"
+            key={item.id}>
+            <input type="checkbox"
+              checked={item.isDone}
+              onChange={() => { onToggleItem(item.id) }}
+            />
+            <input type="text" placeholder='Add a new todo'
+              value={item.text}
+              onChange={(e) => onTextChange(item.id, e.target.value)}
+              style={{ textDecoration: item.isDone ? 'line-through' : 'none' }} />
+          </div>
+        })}
+      </div>
     </main>
     <footer>Made with 💜</footer>
-    </>
+  </>
 
 }
 
