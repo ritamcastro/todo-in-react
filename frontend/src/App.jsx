e991d4283e10ee44f776036cb502b38105a7b99c
Rita Castro <ritamcastro@gmail.com>
2024-10-22 18:36:37 +0100

    🚨 Fixed some linting errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from 'react'
 
 const App = () => {
-
   // As a first approach, let's save on the browser's localStorage for simplicity
   const [items, setItems] = useState(() => {
     const savedItems = localStorage.getItem('todoItems')
@@ -12,16 +11,15 @@ const App = () => {
     localStorage.setItem('todoItems', JSON.stringify(items))
   }, [items])
 
-
   // Attention on () and returns, thanks GPT! ;)
   const onTextChange = (id, value) => {
-    const updatedItems = items.map((item) => (item.id === id ? { ...item, text: value } : item))
+    const updatedItems = items.map(item => (item.id === id ? { ...item, text: value } : item))
 
     setItems(updatedItems)
   }
 
-  const onToggleItem = (id) => {
-    const updatedItems = items.map((item) => {
+  const onToggleItem = id => {
+    const updatedItems = items.map(item => {
       return item.id === id ? { ...item, isDone: !item.isDone } : item
     })
     setItems(updatedItems)
@@ -33,42 +31,57 @@ const App = () => {
   }
 
   const addTodo = () => {
-    setItems([...items, { id: Date.now(), isDone: false, text: "" }])
+    setItems([...items, { id: Date.now(), isDone: false, text: '' }])
   }
 
   const sortedItems = [...items].sort((a, b) => a.isDone - b.isDone)
 
-  return <>
-    <header>To-Do in React</header>
-    <main>
-      <button onClick={() => addTodo()}>New</button>
-      <div role="list">
-        {sortedItems.map((item) => {
-          return <div role="listitem"
-            key={item.id}>
-            <input type="checkbox"
-              checked={item.isDone}
-              onChange={() => { onToggleItem(item.id) }}
-            />
-            <input type="text" placeholder='Add a new todo'
-              value={item.text}
-              onChange={(e) => onTextChange(item.id, e.target.value)}
-              style={{ textDecoration: item.isDone ? 'line-through' : 'none' }} />
-            <button
-              aria-label="Delete"
-              type="button"
-              onClick={() => onDeleteItem(item.id)}
-            >
-              🗑️
-            </button>
-          </div>
-
-        })}
-      </div>
-    </main>
-    <footer>Made with 💜</footer>
-  </>
-
+  return (
+    <>
+      <header>To-Do in React</header>
+      <main>
+        <button
+          type="button"
+          onClick={() => addTodo()}
+        >
+          New
+        </button>
+        <div role="list">
+          {sortedItems.map(item => {
+            return (
+              <div
+                role="listitem"
+                key={item.id}
+              >
+                <input
+                  type="checkbox"
+                  checked={item.isDone}
+                  onChange={() => {
+                    onToggleItem(item.id)
+                  }}
+                />
+                <input
+                  type="text"
+                  placeholder="Add a new todo"
+                  value={item.text}
+                  onChange={e => onTextChange(item.id, e.target.value)}
+                  style={{ textDecoration: item.isDone ? 'line-through' : 'none' }}
+                />
+                <button
+                  aria-label="Delete"
+                  type="button"
+                  onClick={() => onDeleteItem(item.id)}
+                >
+                  🗑️
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      </main>
+      <footer>Made with 💜</footer>
+    </>
+  )
 }
 
 // The same as this one ;)
