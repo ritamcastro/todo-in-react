8693c5bfd25f0f9844201fdc05915aa3bdc8afb3
Rita Castro <ritamcastro@gmail.com>
2024-10-23 16:04:08 +0100

    ♿️ Fix a11y issues found with axe

--- frontend/src/ui/molecules/inline-card.jsx
+++ frontend/src/ui/molecules/inline-card.jsx
@@ -1,43 +1,47 @@
 import { useNavigate } from 'react-router-dom'
 
 const InlineCard = ({ item, onToggle, onChangeText, onDelete }) => {
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  return (
-    <div
-      role="listitem"
-      key={item.id}
-    >
-      <input
-        type="checkbox"
-        checked={item.isDone}
-        onChange={() => {
-          onToggle()
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Add a new todo"
-        value={item.text}
-        onChange={e => onChangeText(e.target.value)}
-        style={{ textDecoration: item.isDone ? 'line-through' : 'none' }}
-      />
-      <button
-        aria-label="See more"
-        type="button"
-        onClick={() => navigate(`/details/${item.id}`)}
-      >
-        👀
-      </button>
-      <button
-        aria-label="Delete"
-        type="button"
-        onClick={() => onDelete(item.id)}
-      >
-        🗑️
-      </button>
-    </div>
-  )
+    const ariaLabelForCheckbox = `Mark todo "${item.text}" as ${item.isDone ? "done" : "not done"}`
+
+    return (
+        <div
+            role="listitem"
+            key={item.id}
+        >
+            <input
+                aria-checked={item.isDone}
+                aria-label={ariaLabelForCheckbox}
+                type="checkbox"
+                checked={item.isDone}
+                onChange={() => {
+                    onToggle()
+                }}
+            />
+            <input
+                type="text"
+                placeholder="Add a new todo"
+                value={item.text}
+                onChange={e => onChangeText(e.target.value)}
+                style={{ textDecoration: item.isDone ? 'line-through' : 'none' }}
+            />
+            <button
+                aria-label="See more"
+                type="button"
+                onClick={() => navigate(`/details/${item.id}`)}
+            >
+                👀
+            </button>
+            <button
+                aria-label="Delete"
+                type="button"
+                onClick={() => onDelete(item.id)}
+            >
+                🗑️
+            </button>
+        </div>
+    )
 }
 
 export default InlineCard
