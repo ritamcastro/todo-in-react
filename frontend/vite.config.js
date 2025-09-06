30a1a11450a584ad48bde7ba1ea36d03641aff3b
Rita Castro <ritamcastro@gmail.com>
2025-09-06 19:13:20 +0100

    🚀 Add scripts to deploy to github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({  
+  base: process.env.NODE_ENV === 'production' ? '/todo-in-react/' : '/',
   plugins: [react()],
    build: {
     // This will make the build fail if there are TypeScript errors
